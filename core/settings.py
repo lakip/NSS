c06d95b9b9cc8dd0ea8df3e72c4e20cc382dea95
ARON LANG'AT <aronlakip@gmail.com>
2020-02-13 20:22:00 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'NSC',
-        'USER': 'postgres',
-        'PASSWORD': 'aron',
-        'HOST': '127.0.0.1',
-        'PORT': '5433',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
